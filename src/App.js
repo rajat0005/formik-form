ac2fca0f7a363ef1d63fea9fdea566ce80de7920
rajat kamra <rkamra94@gmail.com>
2020-07-02 12:14:33 +0530

    basic setup complete without create-react-app

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () =>
+  <div>
+    SIMPLE PARCEL APP
+    </div>
+
+
+export default App
---
+++ src/Index.js
@@ -0,0 +1,5 @@
+import reactDOM from 'react-dom'
+import App from './App'
+import React from 'react'
+
+reactDOM.render(<App />, document.getElementById('root'))
